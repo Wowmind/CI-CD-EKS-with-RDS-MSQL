d426af96ee47da5970609e73b6eaf377594b1f69
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2025-06-11 18:30:12 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,26 +1,26 @@
-
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4" # or current version
+
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  cluster_role_arn = aws_iam_role.eks_cluster_role.arn
+  enable_irsa = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
       instance_types = ["t3.medium"]
-      iam_role_arn   = aws_iam_role.eks_node_role.arn
+      subnet_ids     = module.vpc.private_subnets
     }
   }
-
-  manage_aws_auth_configmap = true
 }
+
 
 
 
